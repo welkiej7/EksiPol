9cb1d3b278745696a11ba6a9392c1a47039f6786
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2024-10-13 22:11:21 +0200

    Add files via upload

---
+++ Follower_Following_Code/program.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+python moral_panic.py
---
+++ Follower_Following_Code/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install selenium
+pip install bs4
+pip install html5lib
+pip install tqdm
+pip install logging
+
+
+
