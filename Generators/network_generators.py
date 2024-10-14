e122156124d185ac8461e20594adb242836a406a
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2024-10-14 23:09:35 +0200

    Add files via upload

--- Generators/network_generators.py
+++ Generators/network_generators.py
@@ -5,7 +5,9 @@ from user_space_generators import generate_user_space_gaussian, generate_user_sp
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-
+import sys
+from PIL import Image
+import os 
 
 class SEQ_GENERATOR():
     
@@ -79,10 +81,62 @@ class SEQ_GENERATOR():
                 conns = np.random.choice(range(len(CANDIDATES)), size = TOT_CAST, replace = False, p = CANDIDATES)
                 for _ in conns:
                     EDGES.append((NODE,_))
-
+                
             self.network = ig.Graph(len(self.user_similarity_matrix[NODE]), EDGES, directed = False).simplify(multiple = True)
+    
+    def generate_a_retweet_network(self, TOPIC_NO:int, ECHO_CHAMBER_STRENGTH:float, AVG_RETWEET:float = 1, SD_RETWEET:float = 0.5, HPOW:float = 100):
+        '''
+        Generates a retweet network from an already existing network embedded in the graph. The number of retweet a user makes
+        is sampled either from a normal distribution or a power law distribution. 
+
+        @param ECHO_CHAMBER_STRENGTH:float Probability of a user retweeting from her follower list.
+        @param HASHTAG_PROB:float Probability of a user retweeting from any user that is similar to her.
+        @param AVG_RETWEET: If using a normal distribution, average number of retweet a user will make.
+        @param SD_RETWEET: If using a normal distribution, standard deviation of the distribution.
+        '''
+
+
+        if not hasattr(self, 'network'):
+            raise AttributeError(f'Object does not contain a graph embedded:\n{self}')
         
-    def PLOT(self,TOPIC_NO):
+
+
+        # Find the number of retweets per user. 
+
+        EXPECTED_RETWEET_COUNT = np.round(np.random.normal(AVG_RETWEET, SD_RETWEET, size = self.user_similarity_matrix.shape[0]))
+        HASHTAG_PROB = 1 - ECHO_CHAMBER_STRENGTH
+        EDGE_LIST = []
+        for USER in tqdm(range(self.user_similarity_matrix.shape[0]), desc = 'Retweeting...'):
+            TMP_RETWEET_COUNT = EXPECTED_RETWEET_COUNT[USER]
+            for RETWEET in range(int(TMP_RETWEET_COUNT)):
+                OBSERVATION = np.random.choice(['F','H'], size = int(TMP_RETWEET_COUNT), replace = True, p = [ECHO_CHAMBER_STRENGTH,HASHTAG_PROB])
+                for CAST in OBSERVATION:
+                    if CAST == 'H':
+                        RETWEET_CANDIDATES = list(self.user_space[:,TOPIC_NO])
+                        RETWEET_CANDIDATES = 1 - abs(RETWEET_CANDIDATES - RETWEET_CANDIDATES[USER])
+
+                        RETWEET_CANDIDATES[USER] = 0 # Prevent self loops
+                        RETWEET_CANDIDATES = [0 if i<0 else i for i in RETWEET_CANDIDATES]
+                        RETWEET_CANDIDATES = [0.99 if i == 1 else i for i in RETWEET_CANDIDATES]
+                        
+            
+                        CAST = np.random.choice(RETWEET_CANDIDATES, size = 1, p = (np.array(RETWEET_CANDIDATES)**HPOW) / np.sum(np.array(RETWEET_CANDIDATES)**HPOW))
+                        CASTED_NODE = np.where(np.array(RETWEET_CANDIDATES) == CAST)[0]
+                        if len(CASTED_NODE) == 0:
+                            continue
+                        else:
+                            EDGE_LIST.append((USER,CASTED_NODE[0]))
+                    if CAST == 'F':
+                        RETWEET_CANDIDATES = self.network.neighborhood(USER)
+
+                        CASTED_NODE = np.random.choice(RETWEET_CANDIDATES, size = 1, replace = False)
+                        EDGE_LIST.append((USER,CASTED_NODE.item()))
+    
+        self.retweet_network = ig.Graph(self.user_similarity_matrix.shape[0], EDGE_LIST, directed = True).simplify(loops = True)
+
+
+
+    def PLOT(self,TOPIC_NO, layout = 'kk'):
 
         colormap = plt.cm.viridis
         vertex_colors = [colormap(value) for value in self.user_space[:,TOPIC_NO]]
@@ -90,3 +144,45 @@ class SEQ_GENERATOR():
         self.network.vs['color'] = vertex_colors_hex
         self.network.vs["label"] = [i for i in range(self.network.vcount())]
         self.layout = self.network.layout_kamada_kawai()
+
+    def GIF_IT(self, TOPIC_NO, OUTPUT_FILE_NAME, original_network = True):
+        if original_network:
+            EDGE_LIST = self.network.get_edgelist()
+        else:
+            EDGE_LIST = self.retweet_network.get_edgelist()
+        os.system('mkdir GIF')
+        
+        for _ in range(len(EDGE_LIST)):
+            GRAPH = ig.Graph(self.user_similarity_matrix.shape[0], EDGE_LIST[0:_])
+            colormap = plt.cm.viridis
+            vertex_colors = [colormap(value) for value in self.user_space[:,TOPIC_NO]]
+            vertex_colors_hex = [mcolors.to_hex(color) for color in vertex_colors]
+            self.network.vs['color'] = vertex_colors_hex
+            self.network.vs["label"] = [i for i in range(self.network.vcount())]
+            self.layout = self.network.layout_kamada_kawai()
+            
+
+            ig.plot(GRAPH, vertex_color = self.network.vs['color'], vertex_label = self.network.vs["label"], layout = self.layout, target= f"GIF/{_}.png")
+            last = _
+        
+        if _ == len(EDGE_LIST) - 1:
+            for TOPIC in range(self.user_space.shape[1]):
+                colormap = plt.cm.magma
+                vertex_colors = [colormap(value) for value in self.user_space[:,TOPIC]]
+                vertex_colors_hex = [mcolors.to_hex(color) for color in vertex_colors]
+                self.network.vs['color'] = vertex_colors_hex
+                self.network.vs["label"] = [i for i in range(self.network.vcount())]
+                ig.plot(GRAPH, vertex_color = self.network.vs['color'], vertex_label = self.network.vs["label"], layout = self.layout, target= f"GIF/{last + TOPIC}.png")
+
+
+        FRAMES = [f"GIF/{_}.png" for _ in range(len(EDGE_LIST) + self.user_space.shape[1] - 1)]
+        GIF_FRAMES = []
+        for i in FRAMES:
+            GIF_FRAMES.append(Image.open(i))
+
+        GIF_FRAMES[0].save(OUTPUT_FILE_NAME, save_all = True, append_images = GIF_FRAMES[0:], duration = 100, loop = 0)
+        
+        os.system('rm -r GIF')
+
+    def CALCULATE_SEPERATION(self,TOPIC):
+        return np.mean([abs(self.user_space[x,TOPIC] - self.user_space[y,TOPIC]) for (x,y) in self.retweet_network.get_edgelist()])
